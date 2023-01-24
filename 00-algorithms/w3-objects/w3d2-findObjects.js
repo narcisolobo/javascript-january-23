a5944e49bb1a694c4f1a29f1c8e2d5aced7d2a61
narcisolobo <narcisolobo@gmail.com>
2023-01-24 07:19:09 -0800

    added w3d2 algos

---
+++ 00-algorithms/w3-objects/w3d2-findObjects.js
@@ -0,0 +1,60 @@
+/* 
+  Given:
+    - a search criteria object with primitive values only (ints,
+      strings, booleans)
+    - a list of objects
+
+  Return any object that matches all the key value pairs in the search
+  criteria object.
+
+  Bonus: write a 2nd solution using built-in methods to practice functional
+  programming.
+*/
+
+const items = [
+  { firstName: "Bob", lastName: "Bobbert", age: 31 },
+  { firstName: "John", lastName: "Smith", age: 25 },
+  { firstName: "Bob", lastName: "Smith", age: 27 },
+  { firstName: "Bob", lastName: "White", age: 31 },
+];
+
+const searchCriteria1 = {
+  firstName: "Bob",
+  age: 31,
+};
+
+const expected1 = [
+  { firstName: "Bob", lastName: "Bobbert", age: 31 },
+  { firstName: "Bob", lastName: "White", age: 31 },
+];
+
+const searchCriteria2 = {
+  lastName: "Smith",
+};
+
+const expected2 = [
+  { firstName: "John", lastName: "Smith", age: 25 },
+  { firstName: "Bob", lastName: "Smith", age: 27 },
+];
+
+/**
+ * Finds the objects that match the given search criteria.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {Object} criteria
+ * @param {Array<Object>} collection
+ * @returns {Array<Object>} The found objects.
+ */
+function findObjects(criteria, collection) {
+  // your code here
+}
+
+/**
+ * - Time: O(?).
+ * - Space: O(?).
+ */
+function findObjectsFunctional(criteria, collection) {
+  // your code here
+}
+
+export { findObjects, functionalFindObjects };
